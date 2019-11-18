b0d9cd9876d1c2441a4e4fe3cb7a8889ddfb76b6
Laura Flaquer <laura.flaquer@outlook.fr>
2019-11-18 23:26:48 +0100

    part 2 typescript lab

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express = require('express')
+import path = require('path')
+import { MetricsHandler } from './metrics'
+
+const app = express()
+const port: string = process.env.PORT || '8080'
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('views', __dirname + "/view")
+app.set('view engine', 'ejs');
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.get(
+    '/hello/:name', 
+    (req, res) => res.render('hello.ejs', {name: req.params.name})
+  )
+
+app.get('/metrics.json', (req: any, res: any) => {
+    MetricsHandler.get((err: Error | null, result?: any) => {
+      if (err) {
+        throw err
+      }
+      res.json(result)
+    })
+  })
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`server is listening on port ${port}`)
+})
